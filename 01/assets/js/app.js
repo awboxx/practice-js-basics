13484c2f52cc4f93c163537cee38dce9c6ff5092
Anna Wasielewska <awboxx@gmail.com>
2024-01-04 19:40:35 +0100

    Task 01 completed

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -2,3 +2,127 @@ const a = '4.2';
 const b = 9;
 
 console.log(a, b);
+
+console.log(typeof a, typeof b);
+
+const res1 = a + b;
+console.log(res1);
+
+if(res1 > 20) {
+    result = "more"
+}
+else {
+    result = "less"
+}
+console.log(result);
+
+const c = parseFloat(a);
+const res2 = c + b;
+console.log(res2);
+
+if(res2 > 20) {
+    result = "more"
+}
+else {
+    result = "less"
+}
+console.log(result);
+
+
+const res3 = a - b;
+console.log(res3);
+
+if(res3 > 20) {
+    result = "more"
+}
+else {
+    result = "less"
+}
+console.log(result);
+
+
+const res4 = a * b;
+console.log(res4);
+
+if(res4 > 20) {
+    result = "more"
+}
+else {
+    result = "less"
+}
+console.log(result);
+
+
+const res5 = a / b;
+console.log(res5);
+
+if(res5 > 20) {
+    result = "more"
+}
+else {
+    result = "less"
+}
+console.log(result);
+
+
+const res6 = a % b;
+console.log(res6);
+
+if(res6 > 20) {
+    result = "more"
+}
+else {
+    result = "less"
+}
+console.log(result);
+
+
+let d = 7.5;
+const res7 = d++; //console wyświetli 7.5, ale po wykonaniu kodu wynik to 8.5
+console.log(res7);
+
+if(res7 > 20) {
+    result = "more"
+}
+else {
+    result = "less"
+}
+console.log(result);
+
+
+const res8 = ++d; //tu zmienna d ma wartość 8.5, dlatego console wyświetli 9.5
+console.log(res8);
+
+if(res8 > 20) {
+    result = "more"
+}
+else {
+    result = "less"
+}
+console.log(result);
+
+
+const res9 = d--; //tu zmienna d ma wartość 9.5, dlatego console wyświetli 9.5, ale wynik działania kodu to to 8.5
+console.log(res9);
+
+if(res9 > 20) {
+    result = "more"
+}
+else {
+    result = "less"
+}
+console.log(result);
+
+
+const res10 = --d; //tu zmienna d ma wartość 8.5, dlatego console wyświetli 7.5
+console.log(res10);
+
+if(res10 > 20) {
+    result = "more"
+}
+else {
+    result = "less"
+}
+console.log(result);
+
+
